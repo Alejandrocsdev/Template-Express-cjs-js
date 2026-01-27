0db5c6df930ffc2589eb616348cccaebfd33dd06
Alejandrocsdev <alejandrocsdev@gmail.com>
2026-01-27 08:14:53 +0800

    add handlebars template engine

--- server.js
+++ server.js
@@ -4,12 +4,24 @@ const express = require('express');
 const app = express();
 const port = Number(process.env.PORT) || 3000;
 
+const { engine } = require('express-handlebars');
+
 const { notFound, errorHandler } = require('./middlewares');
 
 const routes = require('./routes');
 
+// Template Engine
+app.engine('.hbs', engine({ extname: '.hbs' }));
+app.set('view engine', '.hbs');
+app.set('views', './views');
+
+// Static Files
+app.use(express.static('public'));
+
 // Routes
 app.get('/', (req, res) => res.json({ status: 'ok' }));
+app.get('/favicon.ico', (req, res) => res.status(204));
+app.get('/home', (req, res) => res.render('home'));
 app.use('/api', routes);
 
 // Error handling
